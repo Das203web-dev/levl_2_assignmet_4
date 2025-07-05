bd67dabdb014134bc3b817f87ee7e858e1edbbf6
Shuvajit Kumar Das <shuvajitdas838@gmail.com>
2025-07-05 18:37:27 +0600

    changed the baseapi link

--- src/redux/Api/baseApi.ts
+++ src/redux/Api/baseApi.ts
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
     reducerPath: "baseApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
-    // baseQuery: fetchBaseQuery({ baseUrl: "https://level-2-assignment-3-6lyx.onrender.com/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://level-2-assignment-3-6lyx.onrender.com/" }),
     tagTypes: ["books", "borrowBooks"],
     endpoints: (builder) => ({
         getBooks: builder.query({
